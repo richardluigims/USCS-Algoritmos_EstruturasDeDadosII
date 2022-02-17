0e27a9058cf017fe3ec2420ccc100d460882b36c
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-02-16 21:22:04 -0300

    Update Triangulo_If-For-Array.c

--- Aula2/Revisao/Triangulo_If-For-Array.c
+++ Aula2/Revisao/Triangulo_If-For-Array.c
@@ -9,15 +9,15 @@ Autor: Richard Mantoan
 
 int main()
 {
-    int i, tamanho = 3;
+    int i, tamanho = 2;
     int x[tamanho];
     
     printf ("Vamos montar um triângulo?\n");
     printf ("\n");
     
-    for (i = 1; i <= tamanho; i++)
+    for (i = 0; i <= tamanho; i++)
     {
-        printf ("Insira o valor para o lado %d: ", i);
+        printf ("Insira o valor para o lado %d: ", i + 1);
         scanf ("%d", &x[i]);
     }  
     
