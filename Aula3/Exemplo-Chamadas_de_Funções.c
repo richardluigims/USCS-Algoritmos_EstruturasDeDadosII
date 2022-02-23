149cafdb9452ff435456f48b8e3f9cb77ad92cb2
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-02-23 20:39:39 -0300

    Update Exemplo-Chamadas_de_Funções.c

--- Aula3/Exemplo-Chamadas_de_Funções.c
+++ Aula3/Exemplo-Chamadas_de_Funções.c
@@ -6,6 +6,15 @@ Data: 23/02/2022
 *******************************************************************************/
 #include <stdio.h>
 
+int main3(); // Sinaliza que a função está no código
+
+int main()
+{
+    printf("Hello World");
+    main3();
+    return 0;
+}
+
 int main2()
 {
     printf("\nHello World 2");
@@ -16,12 +25,5 @@ int main3()
 {
     printf("\nHello World 3");
     main2();
-    return 0;
-}
-
-int main()
-{
-    printf("Hello World");
-    main3();
     return 0;
 }
