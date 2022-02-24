0dc0411a7089544012849bd725732bf5ab82be88
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-02-23 22:07:18 -0300

    Create Soma_Chamando_Funções.c

---
+++ Aula3/Soma_Chamando_Funções.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+Objetivo: passagem de valores (parametros) para um função
+
+Autor: Richard Mantoan
+Data: 23/02/2022
+
+*******************************************************************************/
+#include <stdio.h>
+
+int soma();
+
+int main()
+{
+    int aux;
+    
+    aux = soma (10, 20);
+    printf("\nA soma = %d", aux);
+    
+    aux = soma (10, 30);
+    printf("\nA soma = %d", aux);
+    
+    aux = soma (1, 2);
+    printf("\nA soma = %d", aux);
+    
+    aux = soma (1000, 2000);
+    printf("\nA soma = %d", aux);
+    
+    return 0;
+}
+
+int soma(int a, int b)
+{
+    return a + b;
+}
