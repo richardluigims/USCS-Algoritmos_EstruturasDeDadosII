3a961888f7d9223bd08994e5c27a0bd9abd9a171
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-04-06 20:41:20 -0300

    Create Struct-Typedef.cs

---
+++ Aula 7/Struct-Typedef.cs
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+Objetivo: exemplo Struct com apelido (typedef)
+
+Autor: Richard
+Data: 06/04/2022
+
+*******************************************************************************/
+#include <stdio.h>
+
+typedef struct Pessoa
+{
+    char nome[30];
+    int idade;
+    float salario;
+} PESSOA; //apelido
+
+int main()
+{
+    PESSOA p;
+    
+    strcpy(p.nome, "Paulo");
+    p.idade = 35;
+    p.salario = 2000;
+    
+    // ou PESSOA p = {"Paulo", 35, 2000}
+    
+    return 0;
+}
+
+/*
+
+sem typedef
+
+    int main()
+    {
+        struct Pessoa p = {"Paulo", 35, 2000};
+    }
+
+*/
